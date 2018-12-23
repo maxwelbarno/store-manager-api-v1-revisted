516ee9f64db2b5391fb215e8165b96c5c8b18822
kipruto <barxwells@gmail.com>
2018-12-24 00:38:57 +0300

    refactoring code duplications

--- app/api/views.py
+++ app/api/views.py
@@ -39,23 +39,13 @@ def user(email, is_admin, password):
     new = User.create_user(email, is_admin, User.generate_hash(password))
     return True
 
-def product_validation(product_name, category, quantity, price):
+def product(product_name, category, quantity, price):
     product_name = product_name
     category=category
     quantity=quantity
     price=price
     validated_product=ValidateProduct.validate(product_name, category, quantity, price)
     return validated_product
-
-def product_create(product_name, category, quantity, price):
-    product_validation(product_name, category, quantity, price)
-    new_product = Product.create_product(product_name, category, quantity, price)
-    return new_product
-
-def product_update(product_name, category, quantity, price):
-    product_validation(product_name, category, quantity, price)
-    product = Product.update_product(product_name, category, quantity, price)
-    return product
 
 def error_handling(error):
     error = error
@@ -108,7 +98,9 @@ class Products(Resource):
         """ Only admin can add a product """
         try:
             data = request.get_json()
-            return make_response(jsonify(product_create(data['product_name'], data['category'], data['quantity'], data['unit_price'])), 201)
+            product(data['product_name'], data['category'], data['quantity'], data['unit_price'])
+            Product.create_product(data['product_name'], data['category'], data['quantity'], data['unit_price'])
+            return make_response(jsonify(), 201)
         except KeyError as error:
             return error_handling(error)
 
@@ -135,10 +127,11 @@ class UpdateProduct(Resource):
 
     @jwt_required
     def put(self, product_id):
-        product = Product.get_specific_product(product_id)
+        Product.get_specific_product(product_id)
         data = request.get_json()
         try:
-            return make_response(jsonify({'message': 'update successful!', 'product': product_update(data['product_name'], data['category'], data['quantity'], data['unit_price'])}), 201)
+            product(data['product_name'], data['category'], data['quantity'], data['unit_price'])
+            return make_response(jsonify({'message': 'update successful!', 'product': Product.update_product(data['product_name'], data['category'], data['quantity'], data['unit_price'])}), 201)
         except KeyError as error:
             return error_handling(error)
 
