ccd315671b2b9c9e06ee2d1504d473cfca3f5323
kipruto <barxwells@gmail.com>
2018-12-23 11:28:09 +0300

    refactoring code smells

--- app/api/validations.py
+++ app/api/validations.py
@@ -2,6 +2,8 @@ from flask import abort
 from .models import *
 import re
 
+def error(message):
+    return abort(400, message)
 
 class ValidateRegistration:
     """ User registration details validations """
@@ -13,49 +15,30 @@ class ValidateRegistration:
 
     def validate(self):
         if self.email == "" or self.is_admin == "" or self.password == "":
-            message = "Sorry, there's an empty user value, please check your input values"
-            abort(400, message)
+            error("Sorry, there's an empty user value, please check your input values")
+            # message = "Sorry, there's an empty user value, please check your input values"
+            # abort(400, message)
 
         for user in users:
             if self.email == user['email']:
-                message = "That email is already registered, please login!"
-                abort(400, message)
+                error("That email is already registered, please login!")
+                # message = "That email is already registered, please login!"
+                # abort(400, message)
 
         if not re.match("^[a-zA-Z0-9!#$&_*?^{}~-]+(\.[a-zA-Z0-9!#$&_*?^{}~-]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-zA-Z]+$", self.email):
-            message = "Please use a valid email address"
-            abort(400, message)
+            error("Please use a valid email address")
+            # message = "Please use a valid email address"
+            # abort(400, message)
 
         if re.match("^[a-zA-Z0-9!#$&_*?^{}~-]+(\.[a-zA-Z0-9!#$&_*?^{}~-]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-zA-Z]+$", self.email) and type(self.is_admin) != bool:
-            message = "is_admin value must be a boolean!"
-            abort(400, message)
+            error("is_admin value must be a boolean!")
+            # message = "is_admin value must be a boolean!"
+            # abort(400, message)
 
         if len(self.password) < 6:
-            message = "password is too short, it should be more than 6 characters!"
-            abort(400, message)
-
-
-# class ValidateLogin:
-#     """ User login validation """
-
-#     def __init__(self, email, password):
-#         self.email = email,
-#         self.password = password
-
-#     def validate(self):
-#         if self.email == "" or self.password == "":
-#             message = "Sorry, there's an empty user value, please check your input values"
-#             abort(400, message)
-#         if type(self.email) != str:
-#             message = "An email adress value must be a string"
-#             abort(400, message)
-
-#         if not re.match("^[a-zA-Z0-9!#$&_*?^{}~-]+(\.[a-zA-Z0-9!#$&_*?^{}~-]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-zA-Z]+$", self.email):
-#             message = "Use a valid email address"
-#             abort(400, message)
-
-#         if type(self.password) != str:
-#             message = "An  value must be a string"
-#             abort(400, message)
+            error("password is too short, it should be more than 6 characters!")
+            # message = "password is too short, it should be more than 6 characters!"
+            # abort(400, message)
 
 
 class ValidateProduct:
@@ -70,36 +53,44 @@ class ValidateProduct:
     def validate(self):
         for product in products:
             if self.product_name == product['product_name'] and self.category == product['category'] and type(self.quantity) == int and type(self.unit_price) == float and self.quantity >= 0 and self.unit_price >= 0:
-                message = "Sorry, such a product already exists, please confirm its category"
-                abort(400, message)
+                error("Sorry, such a product already exists, please confirm its category")
+                # message = "Sorry, such a product already exists, please confirm its category"
+                # abort(400, message)
 
         if self.product_name == "" or self.quantity == "" or self.unit_price == "" or self.category == "":
-            message = "Sorry, there's an empty value, please check your input values"
-            abort(400, message)
+            error("Sorry, there's an empty value, please check your input values")
+            # message = "Sorry, there's an empty value, please check your input values"
+            # abort(400, message)
 
         if type(self.product_name) != str:
-            message = "A product name's value must be a string"
-            abort(400, message)
+            error("A product name's value must be a string")
+            # message = "A product name's value must be a string"
+            # abort(400, message)
 
         if type(self.category) != str:
-            message = "A category's value must be a string"
-            abort(400, message)
+            error("A category's value must be a string")
+            # message = "A category's value must be a string"
+            # abort(400, message)
 
         if type(self.quantity) != int:
-            message = "A quantity's value must be an integer"
-            abort(400, message)
+            error("A quantity's value must be an integer")
+            # message = "A quantity's value must be an integer"
+            # abort(400, message)
 
         if self.quantity <= 0:
-            message = "A quantity's value must be a positive integer"
-            abort(400, message)
+            error("A quantity's value must be a positive integer")
+            # message = "A quantity's value must be a positive integer"
+            # abort(400, message)
 
         if type(self.unit_price) != float:
-            message = "A price's value must be of float data type"
-            abort(400, message)
+            error("A price's value must be of float data type")
+            # message = "A price's value must be of float data type"
+            # abort(400, message)
 
         if self.unit_price <= 0:
-            message = "A price's value must be a positive float"
-            abort(400, message)
+            error("A price's value must be a positive float")
+            # message = "A price's value must be a positive float"
+            # abort(400, message)
 
 
 class ValidateSale:
@@ -113,16 +104,20 @@ class ValidateSale:
     def validate(self):
         # for sale in sales:
         if self.product_id == "" or self.quantity == "":
-            message = "Sorry, there's an empty value, please check your input values"
-            abort(400, message)
+            error("Sorry, there's an empty value, please check your input values")
+            # message = "Sorry, there's an empty value, please check your input values"
+            # abort(400, message)
         if type(self.product_id) != int:
-            message = "A sale id's value must be an int"
-            abort(400, message)
+            error("A sale id's value must be an int")
+            # message = "A sale id's value must be an int"
+            # abort(400, message)
 
         if type(self.quantity) != int:
-            message = "A quantity's value must be an integer"
-            abort(400, message)
+            error("A quantity's value must be an integer")
+            # message = "A quantity's value must be an integer"
+            # abort(400, message)
 
         if self.quantity <= 0:
-            message = "quantity cannot be less than one"
-            abort(400, message)
+            error("quantity cannot be less than one")
+            # message = "quantity cannot be less than one"
+            # abort(400, message)
